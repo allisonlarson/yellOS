ac9cde76c0f2bf3c1b712544c90f000415da9b16
Allie <allisonlarson91@gmail.com>
2016-11-05 12:18:51 -0400

    Initial Commit; "Hello World"
    prints "Hello, world!" on boot.
    Use `make run` to assemble, create the isofile, and run in qemu

---
+++ boot.asm
@@ -0,0 +1,19 @@
+global start
+
+section .text
+bits 32
+start:
+    mov word [0xb8000], 0x7D48 ; H
+    mov word [0xb8002], 0x7D65 ; e
+    mov word [0xb8004], 0x7D6c ; l
+    mov word [0xb8006], 0x7D6c ; l
+    mov word [0xb8008], 0x7D6f ; o
+    mov word [0xb800a], 0x7D2c ; ,
+    mov word [0xb800c], 0x7D20 ;
+    mov word [0xb800e], 0x7D77 ; w
+    mov word [0xb8010], 0x7D6f ; o
+    mov word [0xb8012], 0x7D72 ; r
+    mov word [0xb8014], 0x7D6c ; l
+    mov word [0xb8016], 0x7D64 ; d
+    mov word [0xb8018], 0x7D21 ; !
+    hlt
